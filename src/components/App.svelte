ea32a867df29de5f4020b5fecfe91641999e8089
Mizuho Fukuda <mizuho@Mizuhos-MacBook-Pro.local>
2024-02-11 17:51:59 -0800

    added app svelte

---
+++ src/components/App.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<main>
+    <h1>Median Earnings by College Majors</h1>
+</main>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Assistant:wght@200;400;600;700;800&display=swap');
+    main {
+        text-align: center;
+        font-family: 'Assistant', sans-serif;
+    }
+
+    h1 {
+        font-size: 72px;
+        font-weight: 200;
+    }
+</style>

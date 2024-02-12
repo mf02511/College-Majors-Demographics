1b71765b0a9bd30cd3c634d3ad3b81c59d555757
Mizuho Fukuda <mizuho@Mizuhos-MacBook-Pro.local>
2024-02-11 20:17:50 -0800

    unsuccessful data parsing

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,9 +1,32 @@
 <script>
+import * as d3 from 'd3';
+import { onMount } from 'svelte';
+
+let data = [];
+
+onMount(async () => {
+    const res = await fetch(
+      'https://raw.githubusercontent.com/mf02511/Median-Earnings-by-College-Major/main/src/components/major_earnings.csv',
+    );
+    const csv = await res.text();
+    await d3.csvParse(csv, (d, i, columns) => {
+        let row = {}
+        for (let j = 0; j < 27; j++) {
+            row[columns[j]] = d[columns[j]]
+        }
+        data.push(row)
+    });
+    data=data;
+});
+
+$: console.log(data);
+
 
 </script>
 
 <main>
     <h1>Median Earnings by College Majors</h1>
+    <p></p>
 </main>
 
 <style>
